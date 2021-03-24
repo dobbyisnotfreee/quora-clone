42355aea99ef41799cc7efb71fa39a937c5b3a10
huhjae <vkdnsej129@naver.com>
2021-03-24 19:41:31 +0900

    리액트 깎는 백수

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/userSlice';
+import questionReducer from '../features/questionSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
+    question: questionReducer,
   },
 });
